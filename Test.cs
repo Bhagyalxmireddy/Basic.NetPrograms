639277a652177f6dd30388d99ae9de10763e82a6
Bhagi <puppyreddy1997@gmail.com>
2020-11-20 09:20:27 +0530

    ADDED:Added the basicprograms

---
+++ Test.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyFirstProject
+{
+    class Test
+    {
+        static void Main()
+        {
+            Console.WriteLine("this is first line");
+            Console.Write("this is second line");
+            Console.Write("this is third line");
+            Console.ReadLine();
+            Console.ReadKey();
+        }
+    }
+}
